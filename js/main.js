c01ff376f9bc8984c9e036c37c14953cef1007f5
Denis Radin <denis.radin@gmail.com>
2013-11-10 00:35:43 +0200

    Play/stop images on button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 window.inputToPlay = document.getElementById('inputToPlay');
+window.playButton = document.getElementById('playButton');
 
 window.playURLFromInput = function (e) {
     if (htmltone.player.playing === true) {
-        htmltone.player.stop();
+        window.pauseCodeTone();
     } else {
-        if (htmltone.URL === window.inputToPlay.value){
+        if (htmltone.URL === window.inputToPlay.value) {
+            window.resumeCodeTone();
             htmltone.player.play();
         } else {
             if ((e === undefined || e.keyCode === 13 || e.keyCode === undefined) && window.inputToPlay.value.length > 0) {
-                htmltone.load(window.inputToPlay.value);
+                window.playCodeTone(window.inputToPlay.value);
             }
         }
     }
@@ -19,5 +21,19 @@ window.htmltone = new HTMLTone('.scrolling_pane');
 
 if (window.queryString.url !== undefined) {
     $(window.inputToPlay).val(window.queryString.url)
-    htmltone.load(window.queryString.url);
+    window.playCodeTone(window.queryString.url);
+}
+
+window.playCodeTone = function (url) {
+    htmltone.load(url);
+    window.resumeCodeTone();
+}
+
+window.resumeCodeTone = function (url) {
+    window.playButton.className = 'pause_site';
+}
+
+window.pauseCodeTone = function (url) {
+    window.playButton.className = 'play_site';
+    htmltone.player.stop();
 }
